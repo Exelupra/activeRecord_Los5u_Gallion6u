291f3192a55fb57a3a7e96b01aa50c7417d0ba63
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2022-11-24 09:43:06 +0100

    suite de la classe de test

--- Test/TestFilm.java
+++ Test/TestFilm.java
@@ -1,13 +1,26 @@
 import activeRecord.Film;
+import activeRecord.Personne;
+import activeRecord.RealisateurAbsentException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.SQLException;
 
 public class TestFilm {
 
     @BeforeEach
-    public void BeforeEach() {
+    public void BeforeEach() throws SQLException, RealisateurAbsentException {
+        Film.deleteTable();
         Film.createTable();
+        Film film = new Film("Harry Potter à l'école des sorciers", new Personne("Chris", "Colombus", 1));
+        film.save();
+        Film film2 = new Film("Harry Potter et la chambre des secrets", new Personne("Chris", "Colombus", 1));
+        film2.save();
+        Film film3 = new Film("Harry Potter et le prisonnier d'Azkaban", new Personne("Alfonso", "Cuaron", 1));
+        film3.save();
     }
+
+
 
     @AfterEach
     public void AfterEach() {
