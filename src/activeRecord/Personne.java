c36e8fe7b94782c20a50d33c1dc133cccf891161
Exelen <laura.gallion3@etu.univ-lorraine.fr>
2022-12-08 18:23:49 +0100

    AJout de modification dans Personne.java

--- src/activeRecord/Personne.java
+++ src/activeRecord/Personne.java
@@ -10,19 +10,14 @@ public class Personne {
     String nom;
     String prenom;
 
-    public Personne(String nom, String prenom, Integer id) {
+    public Personne(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
-        this.id = Objects.requireNonNullElse(id, -1);
-    }
-
-    public Personne() {
-        this.nom = "test";
-        this.prenom = "test";
         this.id = -1;
     }
 
-    public List<Personne> findAll() throws SQLException {
+
+    public static List<Personne> findAll() throws SQLException {
         Connection connect = DBConnection.getConnection();
 
         String SQLPrep = "SELECT * FROM Personne;";
@@ -35,7 +30,7 @@ public class Personne {
             String nom = rs.getString("nom");
             String prenom = rs.getString("prenom");
             int id = rs.getInt("id");
-            Personne personne = new Personne(nom, prenom, id);
+            Personne personne = new Personne(nom, prenom);
             personnes = List.of(personne);
 
         }
@@ -43,12 +38,9 @@ public class Personne {
 
     }
 
-    public Personne findById(int Id) throws SQLException {
+    public static Personne findById(int Id) throws SQLException {
         boolean trouve = false;
         Connection connect = DBConnection.getConnection();
-
-
-
         PreparedStatement prep1 = connect.prepareStatement("SELECT * FROM Personne where id=?");
         prep1.setInt(1, Id);
         prep1.execute();
@@ -60,7 +52,7 @@ public class Personne {
             String prenom = rs.getString("prenom");
             int id = rs.getInt("id");
             trouve = true;
-            personne = new Personne(nom, prenom, id);
+            personne = new Personne(nom, prenom);
         }
         if (!trouve) {
             return null;
@@ -69,7 +61,7 @@ public class Personne {
     }
 
 
-    public List<Personne> findByName(String nom) throws SQLException {
+    public static List<Personne> findByName(String nom) throws SQLException {
         Connection connect = DBConnection.getConnection();
 
         PreparedStatement prep1 = connect.prepareStatement("SELECT * FROM Personne where nom=?");
@@ -83,7 +75,7 @@ public class Personne {
             String nomt = rs.getString("nom");
             String prenom = rs.getString("prenom");
             int id = rs.getInt("id");
-            personne = new Personne(nomt, prenom, id);
+            personne = new Personne(nomt, prenom);
             personnes = List.of(personne);
         }
         return personnes;
@@ -93,14 +85,12 @@ public class Personne {
         try {
             Connection connect = DBConnection.getConnection();
             Statement stmt = connect.createStatement();
-            String sql = "CREATE TABLE Personne " +
-                    "(id INTEGER not NULL, " +
-                    " nom VARCHAR(255), " +
-                    " prenom VARCHAR(255), " +
-                    " PRIMARY KEY ( id ))";
-            stmt.executeUpdate(sql);
-            System.out.println("Table creee");
-        } catch (SQLException e) {
+            String createString = "CREATE TABLE Personne ( " + "ID INTEGER  AUTO_INCREMENT, "
+                    + "NOM varchar(40) NOT NULL, " + "PRENOM varchar(40) NOT NULL, " + "PRIMARY KEY (ID))";
+            stmt.executeUpdate(createString);
+            System.out.println("1) creation table Personne\n");
+        }catch (SQLException e) {
+            System.out.println("TA UN PUTAI N DE PROBLEME");
             e.printStackTrace();
         }
     }
@@ -118,14 +108,73 @@ public class Personne {
         }
     }
 
-    public void save() {
+    public void saveNew() {
 
         try {
             Connection connect = DBConnection.getConnection();
             Statement stmt = connect.createStatement();
+            this.id = this.getMaxId() + 1;
             String sql = "INSERT INTO Personne (id, nom, prenom) VALUES (" + this.id + ", '" + this.nom + "', '" + this.prenom + "')";
             stmt.executeUpdate(sql);
             System.out.println("Personne sauvegardee");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void update() {
+
+        try {
+            Connection connect = DBConnection.getConnection();
+            Statement stmt = connect.createStatement();
+            String sql = "UPDATE Personne SET nom = '" + this.nom + "', prenom = '" + this.prenom + "' WHERE id = " + this.id;
+            stmt.executeUpdate(sql);
+            System.out.println("Personne modifiee");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void save() {
+        if (this.id == -1) {
+            this.saveNew();
+        } else {
+            this.update();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getMaxId() throws SQLException {
+        Connection connect = DBConnection.getConnection();
+        Statement stmt = connect.createStatement();
+        String sql = "SELECT MAX(id) FROM Personne";
+        ResultSet rs = stmt.executeQuery(sql);
+        int maxId = 0;
+        while (rs.next()) {
+            maxId = rs.getInt("MAX(id)");
+        }
+        return maxId;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getNom() {
+       return nom;
+    }
+    public String getPrenom() {
+        return this.prenom;
+    }
+    public void delete() {
+        try {
+            Connection connect = DBConnection.getConnection();
+            Statement stmt = connect.createStatement();
+            String sql = "DELETE FROM Personne WHERE id = " + this.id;
+            stmt.executeUpdate(sql);
+            this.id = -1;
+            System.out.println("Personne supprimee");
         } catch (SQLException e) {
             e.printStackTrace();
         }
