04ca46b3619b0ce36818f28685b9b8d46b73bf81
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2022-12-09 19:03:32 +0100

    ajout de javadoc dans la classe de test de DBConnection

--- Test/TestDBConnection.java
+++ Test/TestDBConnection.java
@@ -8,13 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class TestDBConnection {
 
+    /**
+     * Test de la methode getConnection pour vérifier qu'elle utlise bien le patron singleton (une seule instace)
+     */
     @Test
     public void testConnection() {
         Connection dbConnection = DBConnection.getConnection();
         Connection dbConnection2 = DBConnection.getConnection();
+        // on verifie que les deux connexions sont les mêmes
         assertSame(dbConnection, dbConnection2);
     }
 
+    /**
+     * Test de la methode setNonDB pour changer le nom de la base à laquelle on se connecte
+     */
     @Test
     public void testNomDB() {
         Connection connection = DBConnection.getConnection();
